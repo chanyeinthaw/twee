ac23c479d519b2f7a3e285dc812d09b965dd0251
Chan Nyein Thaw <me@chanyeinthaw.com>
2021-05-02 17:32:37 +0630

    add telethon

---
+++ telethon/main.py
@@ -0,0 +1,60 @@
+import configparser
+import json
+import asyncio
+from datetime import date, datetime
+
+from telethon import TelegramClient
+from telethon.errors import SessionPasswordNeededError
+from telethon.tl.functions.channels import JoinChannelRequest
+from telethon.tl.functions.messages import (GetHistoryRequest)
+from telethon.tl.types import (
+    PeerChannel
+)
+from telethon import TelegramClient, events
+
+# some functions to parse json date
+class DateTimeEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, datetime):
+            return o.isoformat()
+
+        if isinstance(o, bytes):
+            return list(o)
+
+        return json.JSONEncoder.default(self, o)
+
+
+# Reading Configs
+config = configparser.ConfigParser()
+config.read("config.ini")
+
+# Setting configuration values
+api_id = config['Telegram']['api_id']
+api_hash = config['Telegram']['api_hash']
+channel_url = config['Telegram']['channel_url']
+
+api_hash = str(api_hash)
+
+client = TelegramClient('twee', api_id, api_hash)
+
+@client.on(events.NewMessage(chats=channel_url))
+async def channel_event_handler(event):
+    if "👉 Click To Tweet 👈" not in event.raw_text:
+        print("Message recevied from Click To Tweet Channel")
+        print("Message: %s" % event.raw_text)
+        # datetime object containing current date and time
+        now = datetime.now()
+        dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+        print("Received Date: ", dt_string)
+        # update status to server
+        print("Tweets sent...\n")
+
+
+async def main():
+    await client.start()
+    print("Telegram Client Created")
+
+    await client.run_until_disconnected()
+
+with client:
+    client.loop.run_until_complete(main())
